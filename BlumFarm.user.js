9882446a09294272e07f3ed922a81c986047cfb7
Tilak Rana Magar <54701580+rajin123-droid@users.noreply.github.com>
2024-09-04 10:47:42 +0545

    Update BlumFarm.user.js

--- BlumFarm.user.js
+++ BlumFarm.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         BlumFarm
-// @version      3.0
+// @version      3.1
 // @namespace    Codevoyger
 // @author       Codevoyger
 // @match        https://telegram.blum.codes/*
